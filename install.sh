f467c58638328e6dfaa3686f4bc9d8f74252ed47
Nick Bair <njbair@gmail.com>
2013-02-05 01:07:37 -0500

    Install script now accepts target path as argument

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-dotfiles_path=~/dotfiles
-backup_path=~/dotfiles_old
+[[ $1 != "" ]] && target=$1 || target=~
+dotfiles_path=$target/dotfiles
+backup_path=$target/dotfiles_old
 files="bashrc bash_extra vimrc vim zshrc oh-my-zsh"
 
 echo -n "Backing up old dotfiles..."
 for file in $files; do
-	if [[ -e ~/.$file ]] || [[ -h ~/.$file ]]; then
+	if [[ -e $target/.$file ]] || [[ -h $target/.$file ]]; then
 		mkdir -p $backup_path
-		mv ~/.$file $backup_path/$file
+		mv $target/.$file $backup_path/$file
 	fi
 done
 echo "Done!"
 
 echo -n "Creating symlinks..."
 for file in $files; do
-	ln -s $dotfiles_path/$file ~/.$file
+	ln -s $dotfiles_path/$file $target/.$file
 done
 echo "Done!"
