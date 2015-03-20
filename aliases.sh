050c09cb16f249180c83802a2449413eabbf39ff
Nick Bair <njbair@gmail.com>
2015-03-20 11:53:00 -0400

    Bash Mac compatibility

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,20 @@
+if [ "$UNAME" = "Darwin" ]; then
+    export LS_OPTS="-G"
+elif [ "$UNAME" = "Linux" ]; then
+    export LS_OPTS="--color=auto --group-directories-first"
+else
+    export LS_OPTS=""
+fi
+
 # file listings should be colored, classified, grouped, & human-readable
-alias ls="ls -Fh --color=auto --group-directories-first"
-alias ll="ls -l"
-alias lla="ls -la"
-alias la="ls -lA"
-alias l="ls -CF"
+alias ls='ls -Fh $LS_OPTS'
+alias ll='ls -l'
+alias lla='ls -la'
+alias la='ls -lA'
+alias l='ls -CF'
 
 # disable word-wrapping in less
-alias less="less -S"
+alias less='less -S'
 
 # "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
@@ -16,8 +24,8 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 alias psx="ps aux | grep $1"
 
 # todo.txt aliases
-alias todo="todo.sh"
-alias t="todo.sh"
+alias todo='todo.sh'
+alias t='todo.sh'
 
 # run grep through a less pipe with full color
 grepless () { grep --color=always "$@" | less; }
