b0a5148294ad6348c2b02988b194e651307ccc62
Nick Bair <njbair@gmail.com>
2015-12-08 15:59:42 -0500

    add tmux default config

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 [[ $1 != "" ]] && target=$1 || target=~
 dotfiles_path=$target/dotfiles
 backup_path=$target/dotfiles_old
-files="bashrc bash vimrc vim zshrc oh-my-zsh irssi gitconfig dircolors mc"
+files="bashrc bash vimrc vim zshrc oh-my-zsh irssi gitconfig dircolors mc tmux.conf"
 
 echo -n "Backing up old dotfiles..."
 for file in $files; do
