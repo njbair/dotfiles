7efd42d42f2a34feca52c6617a3ae2ab899595b2
Nick Bair <njbair@gmail.com>
2016-02-26 13:06:43 -0500

    update installer

--- install.sh
+++ install.sh
@@ -1,21 +1,52 @@
 #!/bin/bash
 
-[[ $1 != "" ]] && target=$1 || target=~
-dotfiles_path=$target/dotfiles
-backup_path=$target/dotfiles_old
-files="bashrc bash vimrc vim zshrc oh-my-zsh irssi gitconfig dircolors mc tmux.conf"
+[[ $1 != "" ]] && dest=$1 || dest=~
+dotfiles_path=$dest/dotfiles
+backup_path=$dest/dotfiles_old
+#files="bashrc bash vimrc vim zshrc oh-my-zsh irssi gitconfig dircolors mc tmux.conf"
+symlinks=(
+#    target                         link
+    "${dotfiles_path}/bashrc        ${dest}/.bashrc"
+    "${dotfiles_path}/bash          ${dest}/.bash"
+    "${dotfiles_path}/vim/vimrc     ${dest}/.vimrc"
+    "${dotfiles_path}/vim           ${dest}/.vim"
+    "${dotfiles_path}/zshrc         ${dest}/.zshrc"
+    "${dotfiles_path}/irssi         ${dest}/.irssi"
+    "${dotfiles_path}/gitconfig     ${dest}/.gitconfig"
+    "${dotfiles_path}/dircolors     ${dest}/.dircolors"
+    "${dotfiles_path}/mc            ${dest}/.mc"
+    "${dotfiles_path}/tmux.conf     ${dest}/.tmux.conf"
+)
 
-echo -n "Backing up old dotfiles..."
-for file in $files; do
-	if [[ -e $target/.$file ]] || [[ -h $target/.$file ]]; then
-		mkdir -p $backup_path
-		mv $target/.$file $backup_path/$file
-	fi
-done
-echo "Done!"
 
-echo -n "Creating symlinks..."
-for file in $files; do
-	ln -s $dotfiles_path/$file $target/.$file
+
+count=0
+while [ "x${symlinks[count]}" != "x" ]; do
+    target=$(echo ${symlinks[count]} | awk '{print $1}')
+    link=$(echo ${symlinks[count]} | awk '{print $2}')
+
+    #echo "${link} -> ${target}"
+
+    if [[ -e $link ]] || [[ -h $link ]]; then
+        mkdir -p $backup_path
+        mv $link $backup_path
+    fi
+
+    ln -s $target $link
+
+    count=$(( $count + 1 ))
 done
-echo "Done!"
+
+#for file in $files; do
+#	if [[ -e $target/.$file ]] || [[ -h $target/.$file ]]; then
+#		mkdir -p $backup_path
+#		mv $target/.$file $backup_path/$file
+#	fi
+#done
+#echo "Done!"
+#
+#echo -n "Creating symlinks..."
+#for file in $files; do
+#	ln -s $dotfiles_path/$file $target/.$file
+#done
+#echo "Done!"
